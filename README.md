770db0a562f533bba1d715e2e016ae83f2623e5c
lewando54 <lewando54@gmail.com>
2020-09-09 21:26:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+#SPOJcounter
+
+Jest to program do zliczania wykonanych zadań na polskim SPOJ'u. 
+Działa on na zasadzie Web Scraping'u, czyli metodzie w której program zachowuje się jak użytkownik przeglądarki i pobiera interesujące go dane z witryny.
+Wystarczy tylko podać nazwę użytkownika, a program sam zrobi resztę.
+
+Na dodatek, można z niego korzystać jak z linuxowego polecenia/skryptu. Przy uruchamianiu można podać argument --help.
---
+++ SPOJcounter.py
@@ -0,0 +1,58 @@
+# coding=UTF-8
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'} 
+if len(sys.argv) - 1 == 1 and sys.argv[1] == "--help":
+    print("Składnia: SPOJcounter.py <użytkownik> -t\n\nOpcja    Działanie\n-t       Wyświetli listę nazw wykonanych zadań bez komunikatu z pytaniem (opcjonalnie)")
+else:
+    def zapytanie(fail):
+        if len(sys.argv) - 1 == 1 and sys.argv[1] == "-t" or fail == True:
+            user = input("Podaj nazwę użytkownika\n")
+        elif len(sys.argv) - 1 == 0:
+            user = input("Podaj nazwę użytkownika\n")
+        elif len(sys.argv) - 1 == 2 and sys.argv[2] != "-t":
+            user = sys.argv[2]
+        else:
+            user = sys.argv[1]
+        try:
+            page = requests.get("https://pl.spoj.com/users/"+user+"/")
+        except:
+            print("Sprawdź połączenie internetowe!")
+            exit()
+        global soup
+        soup = BeautifulSoup(page.content, 'html.parser')
+        global test
+        test = soup.find('h3', {'class':['top-4','text-center']})
+
+    zapytanie(False)
+
+    while test == None:
+        print("Ten użytkownik nie istnieje! Spróbuj ponownie.")
+        zapytanie(True)
+    table = soup.find_all('table')[1]
+    rows = table.find_all('tr')
+    zadania = []
+    ilosc = 0
+    for tr in rows:
+        td = tr.find_all('td')
+        for i in td:
+            ilosc = ilosc + 1
+            zadania.append(i.text)
+            
+    print("Zrobiłeś", ilosc, "zadań na SPOJ'u")
+
+    if len(sys.argv) - 1 == 1 and sys.argv[1] != "-t":
+        pytanie = input("Czy chcesz wyświetlić ich nazwy? (t/n)\n")
+    elif len(sys.argv) - 1 == 2 and sys.argv[2] == "-t":
+       pytanie = "t"
+    elif len(sys.argv) - 1 == 0:
+        pytanie = input("Czy chcesz wyświetlić ich nazwy? (t/n)\n")
+    else:
+        pytanie = "t"
+    if pytanie == "t":
+        print("Oto ich nazwy:")
+        for x in zadania:
+            print(x,", ")
+
